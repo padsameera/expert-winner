3da65a83af909245342df14bf573f1f1f5b72b22
padsameera <34439667+padsameera@users.noreply.github.com>
2017-12-11 18:48:06 +0900

    Create sritalk_register_php.php

---
+++ sritalk_register_php.php
@@ -0,0 +1,16 @@
+<?php
+    $con = mysqli_connect("Mysql@localhost:3306", "root", "hello2174*", "userlogin");
+    
+    $RegisterName = $_POST["RegisterName"];
+    $RegisterUserName = $_POST["RegisterUserName"];
+    $RegisterPassword = $_POST["RegisterPassword"];
+    $RegisterAge = $_POST["RegisterAge"];
+    $statement = mysqli_prepare($con, "INSERT INTO user (RegisterName, RegisterUserName, RegisterPassword, RegisterAge) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "siss", $RegisterName, $RegisterUserName, $RegisterPassword, $RegisterAge);
+    mysqli_stmt_execute($statement);
+    
+    $response = array();
+    $response["success"] = true;  
+    
+    echo json_encode($response);
+?>
